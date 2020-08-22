1ac6a1e5899442abb82b66e8df4483027f384e81
PhilFuster <pafus23@protonmail.com>
2020-08-22 12:57:42 -0400

    Got redirect to work in v6. the code in the book wasn't workin

--- notes.md
+++ notes.md
@@ -1,3 +1,11 @@
 # Chapter 11 Learning React by Eve Porcello & Alex Banks
 
 ## React Router
+
+React Router is a declarative way to define routes for an application.
+
+The book showed us v6 through the experimental build. At time of reading the book (082220) React Router v6 is in beta. With some changes to the API.
+
+Redirect component does not work. The way I got a redirect to work was using the useNavigate hook and calle the navigate function in a useEffect hook. It'll still render the 404 at first but it'll quickly redirect to the link.
+
+I tried both ways of doing React Router configuration. I tried the object definition and the more xml approach. I think I like the way the xml one looks more
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
 import {
   Home,
@@ -57,24 +57,28 @@ function App() {
   // ]);
   // return element;
 
+  let navigate = useNavigate();
   let location = useLocation();
-  console.log(location);
+  useEffect(() => {
+    if (location.pathname === '/services') {
+      navigate('/about/services');
+    }
+  });
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />}>
-        <Route path='services' element={<Services />} />
-        <Route path='history' element={<History />} />
-        <Route path='location' element={<Location />} />
-      </Route>
-      <Route path='/events' element={<Events />} />
-      <Route path='/products' element={<Products />} />
-      <Route path='/contact' element={<Contact />} />
-      {location.pathname === '/services' && (
-        <Navigate to='about/services' replace={true} />
-      )}
-      <Route path='*' element={<Whoops404 />} />
-    </Routes>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />}>
+          <Route path='services' element={<Services />} />
+          <Route path='history' element={<History />} />
+          <Route path='location' element={<Location />} />
+        </Route>
+        <Route path='/events' element={<Events />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<Whoops404 />} />
+      </Routes>
+    </div>
   );
 }
 
